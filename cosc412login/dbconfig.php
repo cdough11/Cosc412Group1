8810ad30e4f1c2e869d5cbe22fd2cc3568033a0c
Austin Farr <afarr2@students.towson.edu>
2017-11-30 21:09:54 -0500

    Added example working PHP script
    One working PHP script

--- cosc412login/dbconfig.php
+++ cosc412login/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 $db_host = "edufitdb.ccfkgwtrrucm.us-east-1.rds.amazonaws.com";  // server to connect to.
-$db_name = "EduFitDB";  // the name of the database.
+$db_name = "EduFit_Data_Source";  // the name of the database.
 $db_user = "cosc412group1";  // mysql username to access the database with.
 $db_pass = "Towson2017";  // mysql password to access the database with.
 $db_table = "Student";    // the table that this script will set up and use.
