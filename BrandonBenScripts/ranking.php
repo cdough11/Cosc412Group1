844148ad81038bb33c0c39626e8ecc1f4557dd9a
benalden1 <32557467+benalden1@users.noreply.github.com>
2017-12-11 23:02:18 -0500

    Almost Finished Scripts (will finish really easy)

---
+++ BrandonBenScripts/ranking.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "edufitdb.ccfkgwtrrucm.us-east-1.rds.amazonaws.com";
+$username = "cosc412group1";
+$password = "Towson2017";
+$dbname = "EduFit_Data_Source";
+$teamName = $_POST["teamName"];
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$query = "SELECT  FirstName, LastName, Total, TeamName FROM Activites INNER JOIN Student ON StudentID = SID WHERE TeamName = '" . $teamName "' GROUP BY LastName, FirstName ORDER BY Total DESC LIMIT 7;"
+$result = mysql_query($query);
+echo [$result];
+
+mysqli_close($conn);
+?>
---
+++ BrandonBenScripts/updateCardio.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "edufitdb.ccfkgwtrrucm.us-east-1.rds.amazonaws.com";
+$username = "cosc412group1";
+$password = "Towson2017";
+$dbname = "EduFit_Data_Source";
+$id = $_POST["id"];
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+// Update Cardio 
+$q1 = ;
+//Update Total
+$q2 = ;
+
+
+// prepare and bind
+mysql_query($q1);
+mysql_query($q1);
+
+mysqli_close($conn);
+?>
